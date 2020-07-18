26d4fe29abcede9a2e23f1c15863516f932a1bd3
Adriano Machado <adrianoricardom@gmail.com>
2020-07-17 21:40:32 -0300

    finish creat appointment flow

--- src/pages/AppointmentCreated/index.tsx
+++ src/pages/AppointmentCreated/index.tsx
@@ -1,7 +1,44 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useCallback, useMemo } from 'react';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/Feather';
+import { format } from 'date-fns';
 
+import {
+  Container,
+  Title,
+  Description,
+  OkButton,
+  OkButtonText,
+} from './styles';
+
+interface RouteParams {
+  date: number;
+}
 const AppointmentCreated: React.FC = () => {
-  return <View />;
+  const { reset } = useNavigation();
+  const { params } = useRoute();
+  const routeParams = params as RouteParams;
+
+  const handleOkPressed = useCallback(() => {
+    reset({
+      routes: [{ name: 'Dashboard' }],
+      index: 0,
+    });
+  }, [reset]);
+
+  const formattedDate = useMemo(() => {
+    return format(routeParams.date, "EEEE', 'MMMM dd'th 'yyyy' on 'HH:mm'h' ");
+  }, [routeParams]);
+  return (
+    <Container>
+      <Icon name="check" size={80} color="#04d363" />
+
+      <Title>Everything is ready!</Title>
+      <Description>{formattedDate}</Description>
+      <OkButton onPress={handleOkPressed}>
+        <OkButtonText>Ok</OkButtonText>
+      </OkButton>
+    </Container>
+  );
 };
 export default AppointmentCreated;
